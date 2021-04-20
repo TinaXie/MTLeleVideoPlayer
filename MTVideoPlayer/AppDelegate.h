8603e1e93504e1136606de881e63bc409ecdada5
xiejc <xiejc@yiche.com>
2021-04-20 14:21:51 +0800

    Initial Commit

---
+++ MTVideoPlayer/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MTVideoPlayer
+//
+//  Created by xiejc on 2021/4/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MTVideoPlayer/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  MTVideoPlayer
+//
+//  Created by xiejc on 2021/4/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
