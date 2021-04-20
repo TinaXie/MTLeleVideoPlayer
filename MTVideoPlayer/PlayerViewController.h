6d959d3142340fdb03e27fafaf6d572112575aa3
xiejc <xiejc@yiche.com>
2021-04-20 16:38:55 +0800

    first commit

---
+++ MTVideoPlayer/PlayerViewController.h
@@ -0,0 +1,16 @@
+//
+//  PlayerViewController.h
+//  MTVideoPlayer
+//
+//  Created by xiejc on 2021/4/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PlayerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
